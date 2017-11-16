e5f3c556c0102d72aaebaf3ac2048416b381909d
Joaquin <joaquin@autotel.co>
2017-11-16 15:42:40 -0500

    fix that events input transforms on driver wouln't affect the event

--- hardware/driver-x28v0.js
+++ hardware/driver-x28v0.js
@@ -313,18 +313,22 @@ var DriverX28v0=function(environment,properties){
         originalMessage:chd,
         hardware:tHardware
       }
+      event.data=Array.from(event.data);
       // if(event.type=="matrixButtonVelocity"){
       //   event.data[1]=event.data[1]|(event.data[2]<<8);
       // }
       if(event.type=="matrixButtonPressed"){
+        // console.log("rr");
         matrixButtonsBitmap|=1<<event.data[0];
         event.data[2]=matrixButtonsBitmap;
         event.data[3]=0;
+        // console.log(matrixButtonsBitmap);
       }
       if(event.type=="matrixButtonReleased"){
         matrixButtonsBitmap&=~(1<<event.data[0]);
         event.data[2]=matrixButtonsBitmap;
         event.data[3]=0;
+        // console.log(matrixButtonsBitmap);
       }
       if((/button/i).test(event.type)){
         event.button=event.data[0];
@@ -335,14 +339,14 @@ var DriverX28v0=function(environment,properties){
       myInteractionPattern.handle('interaction',event);
       // myInteractionPattern.on('interaction',console.log);
       //convert encoder scrolls to signed (it can only be -1 or -2)
-      event.data=Array.from(event.data);
+
       if(event.type=="encoderScrolled"){
         event.data[0]=(event.data[0]==0xFF?-1:event.data[0]);
         event.data[0]=(event.data[0]==0xFE?-2:event.data[0]);
         event.delta=event.data[0];
       }
 
-    // console.log(event);
+      console.log(event);
       myInteractionPattern.handle(event.type,event);
     }
   }
