e1112b0a2ab57f7fb27523087298ff8d082e0f68
Joaquin <joaquin@autotel.co>
2018-01-14 12:57:30 +0200

    copy firmware, adapt matrix button reading

---
+++ firmware/x28v2/Midi.h
@@ -0,0 +1,104 @@
+#include "_name_signals.h"
+#ifndef MIDIH
+#define MIDIH
+class Midi {
+  private:
+    uint8_t waitingMidi = 0;
+    uint8_t expectedMessageLength = 0;
+    void (*CB_midiIn)(uint8_t, uint8_t, uint8_t) = 0;
+    void midiInCallback(uint8_t a , uint8_t b , uint8_t c) {
+      if ( 0 != CB_midiIn ) {
+        (*CB_midiIn)(a, b, c);
+      }
+    }
+    uint8_t getMessageExpectedLength(uint8_t header) {
+      uint8_t fheader = header >> 4;
+      switch (fheader) {
+        case 0b1000: return 3; //noteoff
+        case 0b1001: return 3; //noteon
+        case 0b1010: return 3; //polipressure
+        case 0b1011: return 3; //cchange
+        case 0b1100: return 2; //programchange
+        case 0b1101: return 2; //channelpressure
+        case 0b1110: return 3; //pitch bend
+        case 0b1111: { //system
+            switch (header) {
+              case 0b11110001: return 2; //midi time code
+              case 0b11110010: return 3; //song position
+              case 0b11110011: return 2; //song select
+              case 0b11111000: return 1; //clock
+              case 0b11111010: return 1; //start
+              case 0b11111011: return 1; //continue
+              case 0b11111100: return 1; //stop
+              case 0b11111110: return 1; //keepalive
+              case 0b11111111: return 1; //reset device
+              default: return 1;
+            }
+          }
+        default:
+          return 1;
+      }
+    }
+  public:
+    Midi() {
+    }
+    uint8_t lostBytes = 0;
+    uint8_t lastLostByte = 0;
+    void setup() {
+      Serial1.begin(31250);
+    }
+    void onMidiIn( void (*fpa)(uint8_t, uint8_t, uint8_t)) {
+      CB_midiIn = fpa;
+    }
+    void loop() {
+      //this reception algorhithm doesn't work well yet.|
+      while (Serial1.available()) {
+        //Serial.println("--");
+        //Serial.print(Serial1.peek());
+        uint8_t inbuf [] = {0,0,0};
+        if (expectedMessageLength == 0) {
+          expectedMessageLength = getMessageExpectedLength(Serial1.peek());
+        }
+        if (Serial1.available() >= expectedMessageLength) {
+          for(uint8_t a=0; a<expectedMessageLength; a++){
+            inbuf[a]=Serial1.read();
+          }
+          in(inbuf[0], inbuf[1], inbuf[2]);
+          expectedMessageLength=0;
+        } else {
+          //timeout between bytes
+          if (waitingMidi > 2) {
+            while (Serial1.available() > 0) {
+              uint8_t t = Serial1.read();
+
+              in(t, 0, 0);
+              if (t != 0xfe) {
+                lostBytes++;
+                lastLostByte = t;
+              }
+            }
+            waitingMidi = 0;
+            expectedMessageLength=0;
+          }
+          waitingMidi++;
+          break;
+        }
+
+      }
+    }
+  //maybe this should accept an array instead of three params?
+    void out(uint8_t a, uint8_t b, uint8_t c) {
+      Serial1.write(a);
+      Serial1.write(b);
+      Serial1.write(c);
+    }
+    void in(uint8_t a, uint8_t b, uint8_t c) {
+      /*Serial.println("--");
+      Serial.print(a);
+      Serial.print(b);
+      Serial.print(c);*/
+      midiInCallback(a, b, c);
+    }
+
+};
+#endif
---
+++ firmware/x28v2/x28v2.ino
@@ -0,0 +1,199 @@
+#include <LiquidCrystal.h>
+//TODO remove all use of String
+#include <TimerOne.h>
+#include "_name_signals.h"
+#include "mode_MonoSequencer.h"
+#include "mode_Controller.h"
+#include "x28_LedButtons.h"
+#include "Midi.h"
+#include "PatchBus.h"
+// #include <TBN.h>
+// TBN patchBus;
+PatchBus patchBus;
+LedButtons hardware = LedButtons();
+MonoSequencer sequencerMode = MonoSequencer();
+ControllerMode controllerMode = ControllerMode();
+Midi midi = Midi();
+
+uint8_t engagedMode = 0;
+uint8_t globalMicroStep = 0;
+uint8_t tickLen = 12;
+
+LiquidCrystal lcd(49, 48, 47, 46, 45, 44);
+
+void setup() {
+  //lcd.begin(16, 2);
+  //lcd.print("init");
+  Serial.begin(19200);
+
+  hardware.setup(&lcd);
+  hardware.setButtonCallbacks(onButtonPressed, onButtonReleased);
+  hardware.setEncoderCallbacks(onEncoderScrolled, onEncoderPressed, onEncoderReleased);
+  hardware.setBottomButtonsCallbacks(onBottomButtonPressed,onBottomButtonReleased);
+  midi.setup();
+  midi.onMidiIn(midiInCallback);
+
+  controllerMode.setup(& hardware, & midi, & patchBus);
+  sequencerMode.setup(& hardware, & midi, & patchBus);
+  patchBus.setup();
+  patchBus.addMessageListener(onBusMessageReceived);
+  patchBus.start();
+  // patchBus.onData(onBusMessageReceived);
+  patchBus.addMessageListener(onBusMessageReceived);
+  // patchBus.debug_onTip(test_onTip);
+
+  Timer1.initialize(1500);
+  Timer1.attachInterrupt(onInterrupt);
+
+
+  char tstr[] = "Calculeitor";
+  hardware.lcdPrintA((char&) tstr[0]);
+
+}
+void test_onTip() {
+  if (engagedMode == 0) {
+    char pr[4] = "TIP";
+    hardware.lcdPrintA((char&)pr, 3);
+  }
+}
+
+void onBottomButtonPressed(uint8_t button){
+  if(engagedMode==1){
+    controllerMode.onBottomButtonPressed(button);
+  }
+}
+void onBottomButtonReleased(uint8_t button){
+  if(engagedMode==1){
+    controllerMode.onBottomButtonReleased(button);
+  }
+}
+
+void onButtonPressed(byte button, uint32_t pressedButtonsBitmap) {
+  if (engagedMode == 0) {
+    sequencerMode.onButtonPressed(button, pressedButtonsBitmap);
+  } else {
+    controllerMode.onButtonPressed(button, pressedButtonsBitmap);
+  }
+}
+
+void onButtonReleased(byte button) {
+  if (engagedMode == 0) {
+    sequencerMode.onButtonReleased(button);
+  } else {
+    controllerMode.onButtonReleased(button);
+  }
+}
+
+void onEncoderScrolled(int8_t delta) {
+  if (engagedMode == 0) {
+    sequencerMode.onEncoderScrolled(delta);
+  } else {
+    controllerMode.onEncoderScrolled(delta);
+  }
+}
+void onEncoderPressed() {
+  if (engagedMode == 0) {
+    sequencerMode.onEncoderPressed();
+   // lcd.print("EncPr");
+  } else {
+    controllerMode.onEncoderPressed();
+  }
+}
+void onEncoderReleased() {
+  if (engagedMode == 0) {
+    sequencerMode.onEncoderReleased();
+  } else {
+    controllerMode.onEncoderReleased();
+  }
+}
+void onBusMessageReceived(uint8_t * data, uint8_t len) {
+  sequencerMode.step();
+  char pr[4] = "RCV";
+  hardware.lcdPrintA((char&)pr, 3);
+  sequencerMode.onBusMessageReceived(data, len);
+}
+
+uint8_t test_messageCounter = 0;
+uint8_t test_lastHeader = 0;
+void loop() {
+  if (engagedMode == 0) {
+    controllerMode.checkMessages();
+    if (controllerMode.engagementRequested) {
+      engagedMode = 1;
+      char str[] = "controller mode";
+      hardware.lcdPrintB((char&) str[0]);
+
+
+      lcd.setCursor(0, 1);
+      lcd.write(patchBus.test_count);
+
+    } else {
+      sequencerMode.loop();
+    }
+  } else if (engagedMode == 1) {
+    controllerMode.loop();
+  }
+  hardware.loop();
+  patchBus.loop();
+
+  midi.loop();
+}
+
+void microStep() {
+  globalMicroStep++;
+  if (globalMicroStep >= tickLen) {
+    globalMicroStep = 0;
+    sequencerMode.step();
+  }
+}
+
+void midiInCallback(uint8_t a, uint8_t b, uint8_t c) {
+  test_messageCounter++;
+  test_lastHeader = a;
+  sequencerMode.midiIn(a, b, c);
+  switch (a) {
+    case 250: globalMicroStep = 0; break;
+
+    case 248: {
+        microStep();
+        break;
+      }
+  }
+}
+
+#define divideEncoderRotation 4
+const uint8_t grayToBinary = 0b10110100;
+int8_t enc_last = 0;
+int8_t enc_sub = 0;
+unsigned int encoder0Pos = 0;
+char sign(char x) {
+  return (x > 0) - (x < 0);
+}
+//todo: encoder readout should be part of the library
+void onInterrupt() {
+  //encread turns around as follows: <- 0,1,3,2 ->
+  //upon conversion it will turn as: <- 0,1,2,3 ->
+  int8_t enc_read = (grayToBinary >> ( ( (PINA >> 6) & 0x3) * 2 ) ) & 0x3;
+  if (enc_read != enc_last) {
+    int8_t enc_inc = enc_read - enc_last;
+
+    if (enc_inc > 2) {
+      enc_inc = -1;
+    }
+    if (enc_inc < -2) {
+      enc_inc = +1;
+    }
+
+    enc_sub += enc_inc;
+    if (abs(enc_sub) >= divideEncoderRotation) {
+      encoder0Pos += sign(enc_sub);
+      enc_sub = 0;
+      onEncoderScrolled(enc_inc);
+    }
+    enc_last = enc_read;
+  }
+}
+
+
+
+
