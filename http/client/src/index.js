c27fad74f7bac70d0f0b9f449289f123e8416b58
Joaquin <joaquin@autotel.co>
2018-03-01 14:49:41 +0200

    close the loop for the communication

--- http/client/src/index.js
+++ http/client/src/index.js
@@ -1,1 +1,22 @@
-require('./socketMan.js');
+'use strict'
+//environment.js
+var Observable=require('onhandlers');
+var environment=new(function(){
+  var modules=this.modules=new Set();
+  this.plugins={};
+  var self=this;
+  this.use=function(Proto){
+    console.log("using",Proto.name);
+    this.plugins[Proto.name]=new Proto(self);
+  }
+  this.addModule=function(properties){
+    modules.add(properties)
+  }
+  this.connect=function(properties){
+    properties.origin.connectTo(properties.destination);
+  }
+  return this;
+})();
+
+//index.js
+environment.use(require('./socketMan.js'));
--- http/client/src/socketMan.js
+++ http/client/src/socketMan.js
@@ -1,16 +1,24 @@
 'use strict';
-
+var Observable=require('onhandlers');
 var messageCompressor=new (require('../../shared/MessageCompressor.js'));
 var compress=messageCompressor.compress;
 var decompress=messageCompressor.decompress;
 var globalBindFunction;
 var uniqueArray=[];
-var socketMan=new (function(){
+
+var SocketMan=function(environment){
+  Observable.call(this);
+  var self=this;
   var socket = io();
   socket.on('message',function(msg){
     msg=decompress(msg);
     if(msg.type){
-      messageCallbacks[msg.type](msg);
+      self.handle(msg.type,msg);
+      if(messageCallbacks[msg.type]){
+        messageCallbacks[msg.type](msg);
+      }else{
+        console.log("-no procedure for",msg.type);
+      }
     }else{
       console.log('received malformed message',msg);
     }
@@ -19,11 +27,20 @@ var socketMan=new (function(){
   this.testMessage=function(text){
     socket.send('test',text);
   }
-  return this;
-});
 
-var messageCallbacks={
-  '+ module':function(msg){
-    console.log("NEW MODULE",msg);
+  var messageCallbacks={
+    '+ module':function(msg){
+      console.log("NEW MODULE",msg);
+      environment.addModule(msg);
+    },
+    '+ connection':function(msg){
+      console.log("CONNECT MODULE",msg);
+      environment.connect(msg);
+    }
   }
-}
+
+  return this;
+};
+
+
+module.exports=SocketMan
