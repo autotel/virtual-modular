a853d91ffe0e53dfde75d5f2c21cf67439a93de5
Joaquín <joaquin.aldunate@gmail.com>
2018-10-11 11:25:01 +0300

    reactivate http for upcoming demonstrations

--- build-cmd/index.js
+++ build-cmd/index.js
@@ -5,11 +5,13 @@ var environment = require('./virtualModularEnvironment').environment;
 console.log("W",environment);
 
 var X28Hardware=require('../hardwares-serial/DriverX28v0.js');
+var Http = require('../hardwares-http');
 
 var MidiIO = require('../modules-ext/jazz-MidiIO');
 MidiIO.setMidiInterface(require('./CmdMidiInterface.js'));
 environment.module(MidiIO);
 
+environment.useHardware(Http);
 environment.useHardware(X28Hardware);
 
 environment.handle('created');
