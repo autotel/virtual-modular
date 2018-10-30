52db64e573280a636ae002b2e9652c623f396e55
Joaquín <joaquin.aldunate@gmail.com>
2018-10-30 18:03:22 +0200

    trying to change styles of hardwaresHttp, failed to compile

--- hardwares-http/client/src/ui/index.js
+++ hardwares-http/client/src/ui/index.js
@@ -1,12 +1,12 @@
-var Client=require('./Client.js');
-var Elements=require('./sprites');
-var ForceDirectedGrapher=require('./ForceDirectedGrapher.js');
-module.exports=function(environment){
-  var self=this;
-  var start=function(){
+var Client = require('./Client.js');
+var Elements = require('./sprites');
+var ForceDirectedGrapher = require('./ForceDirectedGrapher.js');
+module.exports = function (environment) {
+  var self = this;
+  var start = function () {
     // console.log("UI START");
-    var forceLayout=self.forceLayout=new ForceDirectedGrapher();
-    var client=new Client();
+    var forceLayout = self.forceLayout = new ForceDirectedGrapher();
+    // var client = new Client();
     var stage = new Konva.Stage({
       container: 'konva',
       width: window.innerWidth,
@@ -16,78 +16,83 @@ module.exports=function(environment){
     var layer = self.konvaLayer = new Konva.Layer();
     stage.add(layer);
 
-    var moduleSprites=[];
+    var moduleSprites = [];
 
-    var anim = new Konva.Animation(function(frame) {
+    var anim = new Konva.Animation(function (frame) {
       var time = frame.time,
         deltaTime = frame.timeDiff,
         frameRate = frame.frameRate;
     }, layer);
     anim.start();
-    environment.on('serverstart',function(){
-      for(var module of moduleSprites){
+    environment.on('serverstart', function () {
+      for (var module of moduleSprites) {
         // module.remove();
       }
     });
-    environment.on('modulereset',function(event){
-      var module=event.module;
-      if(module.sprite){
+    environment.on('modulereset', function (event) {
+      var module = event.module;
+      if (module.sprite) {
         module.sprite.remove();
       }
 
     });
-    environment.on('+module',function(event){
+    environment.on('+module', function (event) {
       console.log('+module');
 
-      var module=event.module;
+      var module = event.module;
       // console.log("SPRIOTE",event);
-      if(moduleSprites[module.properties.unique]){
+      if (moduleSprites[module.properties.unique]) {
         // console.log("FOUND",module.properties.unique,module);
         moduleSprites[module.properties.unique].remove();
       }
       var newSprite;
-      if(Elements[module.properties.kind]){
-        newSprite=new Elements[module.properties.kind](self,module.properties);
-      }else{
-        console.log("No "+module.properties.kind+" module sprite, using generic",Elements);
+      if (Elements[module.properties.kind]) {
+        newSprite = new Elements[module.properties.kind](self, module.properties);
+      } else {
+        console.log("No " + module.properties.kind + " module sprite, using generic", Elements);
         console.log(event);
-        newSprite=new Elements.TestModule(self,module.properties);
+        newSprite = new Elements.TestModule(self, module.properties);
       }
-      moduleSprites[module.properties.unique]=(newSprite);
+      moduleSprites[module.properties.unique] = (newSprite);
 
-      
 
-      module.sprite=newSprite;
+
+      module.sprite = newSprite;
     });
-    environment.on('-module',function(event){
+    environment.on('-module', function (event) {
       console.log('-module');
 
-      var module=event.module;
+      var module = event.module;
       moduleSprites[module.properties.unique].remove();
     });
-    environment.on('~module',function(event){
+    environment.on('~module', function (event) {
       console.log('~module');
 
-      var module=event.module;
+      var module = event.module;
       moduleSprites[module.properties.unique].applyChanges(event);
     });
-    environment.on('+connection',function(event){
+    environment.on('+connection', function (event) {
       console.log("+connection");
       event.origin.sprite.connectTo(event.destination.sprite);
     });
-    environment.on('-connection',function(event){
+    environment.on('-connection', function (event) {
       console.log("+connection");
       event.origin.sprite.disconnectTo(event.destination.sprite);
     });
-    environment.on('>message',function(event){
+    environment.on('>message', function (event) {
       // console.log(event);
-      event.origin.sprite.messageOut(event.destination,event.value);
-      if(event.destination.sprite.messageIn){
-        event.destination.sprite.messageIn(event.origin,event.value);
+      try {
+        event.origin.sprite.messageOut(event.destination, event.value);
+        if (event.destination.sprite.messageIn) {
+          event.destination.sprite.messageIn(event.origin, event.value);
+        }
+      } catch (e) {
+        console.warn("on >message event, event.destination.sprite.messageOut doesn't exist", event);
+        console.error(e);
       }
 
     });
   }
-  window.onload=start;
+  window.onload = start;
 
 }
--- patches/default-patch.js
+++ patches/default-patch.js
@@ -1,77 +1,83 @@
 module.exports=[
   {
+    type: 'ClockGenerator',
+    properties: {
+      name: 'main clock',
+      bpm: 120
+    },
+    outputs: ['global']
+  },{
     type: "MidiIO",
     properties: {
       name: "OUT A",
-      midi: "UM-1"
+      midi: "UM-1",
+      loc:4
     }, outpupts: []
   }, {
     type: "MidiIO",
     properties: {
       name: "OUT B",
-      midi: "chanmap B"
+      midi: "chanmap B", 
     }, outpupts: []
   }, {
     type: "MidiIO",
     properties: {
       name: "OUT C",
-      midi: "chanmap C"
+      midi: "chanmap C",
     }, outpupts: []
   }, {
     type: "MidiIO",
     properties: {
       name: "OUT D",
-      midi: "chanmap D"
+      midi: "chanmap D",
     }, outpupts: []
-  },{
-    type:'ClockGenerator',
-    properties:{
-      name:'main clock',
-      bpm:120
-    },
-    outputs:['global']
   },{
     type:'Harmonizer',
     outputs:['OUT A']
   }, {
     type: 'PresetKit',
     properties: {
-      name: 'main drumkit',
+      name: 'drumkit B',
       autoMap: 'timbre',
       kit: [
         [1, 60, 0, -1]
-      ]
+      ],
     },
     outputs: ['OUT B']
   }, {
     type: 'PresetKit',
     properties: {
-      name: 'main drumkit',
+      name: 'drumkit C',
       autoMap: 'timbre',
       kit: [
         [1, 60, 0, -1]
-      ]
+      ],
     },
     outputs: ['OUT C']
   }, {
     type: 'PresetKit',
     properties: {
-      name: 'main drumkit',
+      name: 'drumkit D',
       autoMap: 'timbre',
       kit: [
         [1, 60, 0, -1]
-      ]
+      ],
     },
     outputs: ['OUT D']
   },{
     type: 'Sequencer',
     properties: {
+      loc:12,
     },
     outputs: ['harmonizer 0']
   }, {
     type: 'Sequencer',
-    properties: {
-    },
+    outputs: ['main drumkit']
+  }, {
+    type: 'Sequencer',
+    outputs: ['main drumkit']
+  }, {
+    type: 'Sequencer',
     outputs: ['main drumkit']
   }, 
 ]
