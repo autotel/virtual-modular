d1d80a786137bd81f983dc3511b76174d73b985e
joaquin <joaquin.aldunate@gmail.com>
2019-11-20 16:02:02 +0200

    enable/disable property in print

--- Dem-modules/Print/index.js
+++ Dem-modules/Print/index.js
@@ -2,13 +2,21 @@ const Base= require("../Base.js");
 let instances=0;
 const Print=function(properties,environment){
     this.preventBus=true;
+    let self=this;
     Base.call(this,properties,environment);
+    this.properties={
+        active:{
+            value:false
+        }
+    }
     this.category="utility";
     this.recordingReceived = function(event) {
+        if(!properties.active.value) return;
         var evM = event.eventMessage;
         console.log(">>rec:",evM.value);
     }
     this.messageReceived = function(event) {
+        if(!self.properties.active.value) return;
         var evM = event.eventMessage;
         console.log(">>evm:",evM.value);
     }
